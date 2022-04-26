8e55c9bbffc9db11e0b84a6dd83ab618dfcf6b37
criaDEgangue <104433111+criaDEgangue@users.noreply.github.com>
2022-04-26 13:57:02 -0300

    Add files via upload

---
+++ php/breve.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EM CONSTRUÇÃO...</title>
+    <link rel="icon" type="image/x-icon" href="/hnz/imagem/sacola.png">
+</head>
+
+<!--TELA DE BREVE... - SITE EM DESENVOLVIMENTO-->
+
+<body>
+    <h1>EM CONSTRUÇÃO...</h1>
+</body>
+
+</html>
