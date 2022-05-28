e31212046cc150e10b2f4af47b31ad7b792b4cd5
cria <promeufarm@gmail.com>
2022-05-28 11:01:03 -0300

    chefes

---
+++ php/bancodedados.php
@@ -0,0 +1,15 @@
+<?php
+
+class bd {
+    private $nomeserver = "localhost";
+    private $user = "root";
+    private $pw = "usbw";
+    private $dbnome = "vendas";
+    private $con;
+
+    public function conectar(){
+
+    }
+
+}
+?>
